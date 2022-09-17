0b7cb4b6eb7e7ae4412e57cda7aaf3ea1a74f109
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2022-09-17 10:00:20 -0300

    feat(authentication): ensure remote authentication calls HttpPostClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,13 @@
+import { HttpPostClient } from 'data/protocols/http'
+
+export class RemoteAuthentication {
+  constructor (
+    private readonly httpPostClient: HttpPostClient,
+    private readonly url: string
+  ) {}
+
+  async auth (): Promise<void> {
+    await this.httpPostClient.post(this.url)
+    return await Promise.resolve()
+  }
+}
